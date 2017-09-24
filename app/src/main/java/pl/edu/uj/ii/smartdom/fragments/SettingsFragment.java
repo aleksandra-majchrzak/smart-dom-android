aece1a438c1fdf6c8f0d0c2b0fc0ca65c36ec61e
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2017-09-24 22:17:09 +0200

    Added libraries licences.

--- app/src/main/java/pl/edu/uj/ii/smartdom/fragments/SettingsFragment.java
+++ app/src/main/java/pl/edu/uj/ii/smartdom/fragments/SettingsFragment.java
@@ -1,8 +1,10 @@
 package pl.edu.uj.ii.smartdom.fragments;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -11,6 +13,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 import pl.edu.uj.ii.smartdom.R;
@@ -67,28 +70,72 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         Authentication auth = ((MainActivity) getActivity()).getAuthentication();
         if (TextUtils.isEmpty(auth.getToken())) {
-            getPreferenceManager().getPreferenceScreen().removePreference(getPreferenceManager().findPreference("second_section_preference"));
             serverPref.setEnabled(true);
         } else {
-            getPreferenceManager().getPreferenceScreen().addPreference(getPreferenceManager().findPreference("second_section_preference"));
             serverPref.setEnabled(false);
         }
+
+        getPreferenceManager().findPreference("butterknife_preference")
+                .setOnPreferenceClickListener(new LicenceOnPreferenceClickListener("Butter Knife",
+                        "https://raw.githubusercontent.com/JakeWharton/butterknife/master/LICENSE.txt"));
+
+        getPreferenceManager().findPreference("support_preference")
+                .setOnPreferenceClickListener(new LicenceOnPreferenceClickListener("Android Support Library",
+                        "https://raw.githubusercontent.com/android/platform_frameworks_support/master/LICENSE.txt"));
+
+        getPreferenceManager().findPreference("colorpickerview_preference")
+                .setOnPreferenceClickListener(new LicenceOnPreferenceClickListener("ColorPickerView",
+                        "https://raw.githubusercontent.com/skydoves/ColorPickerView/master/LICENSE"));
+
+        getPreferenceManager().findPreference("retrofit_preference")
+                .setOnPreferenceClickListener(new LicenceOnPreferenceClickListener("Retrofit",
+                        "https://raw.githubusercontent.com/square/retrofit/master/LICENSE.txt"));
+
+        getPreferenceManager().findPreference("okhttp_preference")
+                .setOnPreferenceClickListener(new LicenceOnPreferenceClickListener("OkHttp",
+                        "https://raw.githubusercontent.com/square/okhttp/master/LICENSE.txt"));
+
+        getPreferenceManager().findPreference("rxandroid_preference")
+                .setOnPreferenceClickListener(new LicenceOnPreferenceClickListener("RxAndroid",
+                        "https://raw.githubusercontent.com/ReactiveX/RxAndroid/2.x/LICENSE"));
+
+        getPreferenceManager().findPreference("rxjava_preference")
+                .setOnPreferenceClickListener(new LicenceOnPreferenceClickListener("RxJava",
+                        "https://raw.githubusercontent.com/ReactiveX/RxJava/2.x/LICENSE"));
+
+        getPreferenceManager().findPreference("sugar_preference")
+                .setOnPreferenceClickListener(new LicenceOnPreferenceClickListener("Sugar",
+                        "https://raw.githubusercontent.com/chennaione/sugar/master/LICENSE"));
 
         return view;
     }
 
-    /*@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    class LicenceOnPreferenceClickListener implements Preference.OnPreferenceClickListener {
 
-        mListPreference = (ListPreference) getPreferenceManager().findPreference("preference_key");
-        mListPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                // your code here
-                return true;
-            }
-        });
+        String libraryName;
+        String URL;
 
-        return inflater.inflate(R.layout.fragment_settings, container, false);
-    }*/
+        LicenceOnPreferenceClickListener(String libraryName, String URL) {
+            this.libraryName = libraryName;
+            this.URL = URL;
+        }
+
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.licence_view, null);
+            WebView licenceWebView = (WebView) view.findViewById(R.id.licence_webview);
+            licenceWebView.loadUrl(URL);
+            new AlertDialog.Builder(getContext())
+                    .setTitle(libraryName)
+                    .setView(view)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            ;
+                        }
+                    }).show();
+            return true;
+        }
+    }
 }
